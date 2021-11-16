129f51c809e62c8bc749e61f0cfeb23eb01c2074
hoppaang <hoppaang@hatiolab.com>
2021-11-16 15:26:46 +0900

    Update edit-toolbar-style.js

--- packages/process-modeller/client/board-modeller/edit-toolbar-style.js
+++ packages/process-modeller/client/board-modeller/edit-toolbar-style.js
@@ -7,6 +7,7 @@ import { css } from 'lit-element'
 export const style = css`
   :host {
     background-color: var(--edit-toolbar-background-color, #394e64);
+    overflow: hidden;
   }
 
   [tools] {
