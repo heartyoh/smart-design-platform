2a7e9a3f73dab6ef03f7aaf4b1184d2772b00c2e
hoppaang <hoppaang@hatiolab.com>
2021-12-16 15:00:45 +0900

    fix: svg color, icon

--- packages/process-modeller/client/component/burner.js
+++ packages/process-modeller/client/component/burner.js
@@ -1,20 +1,20 @@
 const icon = `
 <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 50 35" style="enable-background:new 0 0 50 35;" xml:space="preserve">
   <style type="text/css">
-    .st5{fill:none;stroke:{{strokeColor}};stroke-miterlimit:10;}
+    .stroke{fill:none;stroke:{{strokeColor}};stroke-miterlimit:10;}
   </style>
   <g>
-    <path class="st5" d="M45.1,29.9v-5c1.7-2.2,2.7-4.9,2.7-7.9c0-7.1-5.7-12.8-12.8-12.8c-0.6,0-1.1,0-1.7,0.1H21.4v0h-3.3V2.9H16v1
+    <path class="stroke" d="M45.1,29.9v-5c1.7-2.2,2.7-4.9,2.7-7.9c0-7.1-5.7-12.8-12.8-12.8c-0.6,0-1.1,0-1.7,0.1H21.4v0h-3.3V2.9H16v1
     H4.8V2.4H2.2V17h2.6v-1.6H16v0.9h2.1V15h3.8v-0.7h0.6c-0.2,0.9-0.3,1.8-0.3,2.7c0,0,0,0.1,0,0.1h-1.9c-0.8,0-1.5,0.7-1.5,1.5v5.8
     c0,0.8,0.7,1.5,1.5,1.5H25v4c-0.6,0.1-1,0.6-1,1.2v0.3c0,0.7,0.5,1.2,1.2,1.2H45c0.7,0,1.2-0.5,1.2-1.2v-0.3
     C46.2,30.5,45.7,30,45.1,29.9z"/>
-    <line class="st5" x1="21.6" y1="4.6" x2="21.6" y2="14.5"/>
-    <line class="st5" x1="4.8" y1="4.4" x2="4.8" y2="15.4"/>
-    <line class="st5" x1="16" y1="4.4" x2="16" y2="15"/>
-    <line class="st5" x1="18.1" y1="4.4" x2="18.1" y2="15"/>
-    <circle class="st5" cx="35.1" cy="16.9" r="12.5"/>
-    <circle class="st5" cx="35.1" cy="16.9" r="10.3"/>
-    <line class="st5" x1="27.4" y1="23.6" x2="42.6" y2="10"/>
+    <line class="stroke" x1="21.6" y1="4.6" x2="21.6" y2="14.5"/>
+    <line class="stroke" x1="4.8" y1="4.4" x2="4.8" y2="15.4"/>
+    <line class="stroke" x1="16" y1="4.4" x2="16" y2="15"/>
+    <line class="stroke" x1="18.1" y1="4.4" x2="18.1" y2="15"/>
+    <circle class="stroke" cx="35.1" cy="16.9" r="12.5"/>
+    <circle class="stroke" cx="35.1" cy="16.9" r="10.3"/>
+    <line class="stroke" x1="27.4" y1="23.6" x2="42.6" y2="10"/>
   </g>
 </svg>
 `
